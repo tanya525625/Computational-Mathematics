dadd5d7518995b3001bdd88446eaebf7affda1da
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2018-02-17 22:29:38 +0300

    Update Header.h

--- Task1 - Theory of errors/Header.h
+++ Task1 - Theory of errors/Header.h
@@ -8,7 +8,7 @@ const double pi = 4.*atan(1.);
 const double c1 = 0.6;
 const double c2 = 0.9;
 const double c3 = 1;
-const double e1 = pow(10, -6) / (3*c1); //Вычисление погрешностей через принцип равных влияний
+const double e1 = pow(10, -6) / (3*c1); 
 const double e2 = pow(10, -6) / (3*c2);
 const double e3 = pow(10, -6) / (3*c3);
 
